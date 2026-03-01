c8ab08882e0f4a82e0f90c3ca384ba6dccb25d76
VinhHV28 <vinhhv28@gmail.com>
2026-03-01 14:30:52 +0700

    modified models atributes

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Customer from "./Customer.js";
 import MenuItem from "./MenuItem.js";
 import User from "./User.js";
 import Table from "./Table.js";
@@ -28,12 +27,9 @@ const orderSchema = new mongoose.Schema(
       enum: ["pending", "preparing", "served", "completed", "cancelled"],
       default: "pending",
     },
-
     items: [orderItemSchema],
-
     totalAmount: { type: Number, default: 0 },
-
-    customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
+    customer: { type: Object },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     table: { type: mongoose.Schema.Types.ObjectId, ref: "Table" },
   },
