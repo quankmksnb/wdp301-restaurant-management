1b9a5f82cee85de521cf032d7ee8dea5c099f5a3
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2026-03-08 00:14:22 +0700

    fix error map of validate middleware

--- backend/src/middlewares/validateMiddleware.js
+++ backend/src/middlewares/validateMiddleware.js
@@ -4,15 +4,22 @@ export const validate = (schema) => (req, res, next) => {
     req.body = data;
     next();
   } catch (error) {
-    const errors = error.errors.map((err) => ({
-      field: err.path[0],
-      message: err.message,
-    }));
+    if (error.issues) {
+      const errors = error.issues.map((err) => ({
+        field: err.path[0],
+        message: err.message,
+      }));
 
-    return res.status(400).json({
+      return res.status(400).json({
+        success: false,
+        message: "Dữ liệu không hợp lệ",
+        errors,
+      });
+    }
+
+    return res.status(500).json({
       success: false,
-      message: "Dữ liệu không hợp lệ",
-      errors,
+      message: "Lỗi validate",
     });
   }
 };
