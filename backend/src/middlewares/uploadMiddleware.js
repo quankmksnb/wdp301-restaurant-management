9784f07caa2f5f1a07463e424b7ea8af4f09f315
VinhHV28 <vinhhv28@gmail.com>
2026-03-23 22:44:49 +0700

    config cloundinary

---
+++ backend/src/middlewares/uploadMiddleware.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cd) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Chỉ chấp nhận file ảnh (jpg, png, webp)"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //Max 5MB
+  },
+  fileFilter,
+});
+
+export default upload;
