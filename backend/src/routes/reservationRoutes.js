2bc5e94b803102894a3377a1fdcce8a3ebaa8697
HoangNV <hoangptkdhy@gmail.com>
2026-03-27 19:26:29 +0700

    authorization backend for reservation

--- backend/src/routes/reservationRoutes.js
+++ backend/src/routes/reservationRoutes.js
@@ -10,7 +10,7 @@ import {
 } from "../controllers/reservationController.js";
 
 import { validate } from "../middlewares/validateMiddleware.js";
-import { verifyToken } from "../middlewares/authMiddleware.js";
+import {authorizeRoles, verifyToken} from "../middlewares/authMiddleware.js";
 
 import {
   createReservationSchema,
@@ -22,6 +22,7 @@ const router = express.Router();
 
 // Tất cả các route về reservation đều cần xác thực
 router.use(verifyToken);
+router.use(authorizeRoles("receptionist"));
 
 router.get("/available-tables", getAvailableTablesController);
 router.get("/reserved-tables", getReservedTablesController);
