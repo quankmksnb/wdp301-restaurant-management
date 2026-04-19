a19e3476dbc55e42b5d9d9706b95c78f940ee562
VinhHV28 <vinhhv28@gmail.com>
2026-04-19 12:11:58 +0700

    fix env for docker

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import { initSocket } from "./configs/socket.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, ".env") });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 await connectDB();
 
